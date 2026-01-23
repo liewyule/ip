e71ea2e18e1466851bee4b68d45cd89c82e0e065
Liew Yu Le <liewyule05@gmail.com>
2026-01-23 12:46:44 +0800

    add new class Task and add mark and unmark function

--- src/main/java/yl.java
+++ src/main/java/yl.java
@@ -5,7 +5,7 @@ public class yl {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         System.out.println("Hello! I'm yl");
         System.out.println("What can I do for you");
@@ -16,12 +16,25 @@ public class yl {
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon !");
                 break;
+
             } else if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + "." + tasks.get(i));
+                    System.out.println((i + 1) + "." + tasks.get(i).toString());
                 }
+            } else if (userInput.startsWith("mark ")) {
+                int num = Integer.parseInt((userInput.split(" ")[1])) - 1;
+                tasks.get(num).mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks.get(num).toString());
+            } else if (userInput.startsWith("unmark ")) {
+                int num = Integer.parseInt((userInput.split(" ")[1])) - 1;
+                tasks.get(num).unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks.get(num).toString());
             } else {
-                tasks.add(userInput);
+                Task task = new Task(userInput);
+                tasks.add(task);
                 System.out.println("added: " + userInput);
             }
         }
