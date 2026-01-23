86b31e304c00f19ebb4e38aa2af102d2c0fa292c
Liew Yu Le <liewyule05@gmail.com>
2026-01-23 11:33:05 +0800

    add support for echo

--- src/main/java/yl.java
+++ src/main/java/yl.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class yl {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm yl");
         System.out.println("What can I do for you");
-        System.out.println("Bye. Hope to see you again soon !");
+
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon !");
+                break;
+            }
+            System.out.println(userInput);
+        }
+
     }
 }
