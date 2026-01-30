a05b5bbb1d969eca850068ea1fd7f087cc022965
Liew Yu Le <liewyule05@gmail.com>
2026-01-30 14:48:39 +0800

    Add Ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,19 @@
+public class Ui {
+    public void printHello() {
+        System.out.println("Hello! I'm yl");
+        System.out.println("What can I do for you");
+        this.printLine();
+    }
+
+    public void printGoodBye() {
+        System.out.println("Bye. Hope to see you again soon !");
+    }
+
+    public void printLine() {
+        System.out.println("---------------------------------------------------");
+    }
+
+    public void printError() {
+        System.out.println("Oh no! something went wrong :(, try again later");
+    }
+}
